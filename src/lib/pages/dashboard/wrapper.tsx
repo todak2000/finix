ddd431f410509d8cbe167d3a9a1ef616625b1f86
Daniel Olagunju <todak2000@gmail.com>
2024-10-08 19:31:13 +0100

    optimizing

--- src/lib/pages/dashboard/wrapper.tsx
+++ src/lib/pages/dashboard/wrapper.tsx
@@ -6,10 +6,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useTheme } from 'next-themes';
 import type { ReactNode } from 'react';
+import { CiPower } from 'react-icons/ci';
 import { RiDashboardLine, RiSettings2Line } from 'react-icons/ri';
+import { useDispatch } from 'react-redux';
 
 import { ThemeToggle } from '@/lib/components/theme-toggle';
 import { useUserSession } from '@/lib/hooks/session';
+import { setModal } from '@/lib/store/slices/modal';
 
 const DashboardWrapper = ({
   children,
@@ -18,12 +21,16 @@ const DashboardWrapper = ({
   children: ReactNode;
   session: string | null;
 }) => {
+  const dispatch = useDispatch();
   const { theme } = useTheme();
+  const handleSignOut = () => {
+    dispatch(setModal({ open: true, type: 'sign-out' }));
+  };
   useUserSession(session);
   const arr = [
     { name: 'Home', icon: <RiDashboardLine />, path: '/dashboard' },
     { name: 'Settings', icon: <RiSettings2Line />, path: '/setting' },
-    // { name: 'Theme', icon: <RiSettings2Line />, path: '/setting' },
+    { name: 'Logout', icon: <CiPower />, path: '/' },
   ];
   return (
     <div className="flex h-screen w-full flex-row justify-between md:overflow-hidden">
@@ -38,18 +45,28 @@ const DashboardWrapper = ({
               className="mb-12 dark:invert"
               loading="lazy"
             />
-            {arr.map((i) => {
-              return (
+            {arr.map((i) =>
+              i.name === 'Logout' ? (
+                <button
+                  key={i.name}
+                  type="button"
+                  onClick={handleSignOut}
+                  className="flex flex-row items-center gap-3 text-black"
+                >
+                  {i.icon}
+                  {i.name}
+                </button>
+              ) : (
                 <Link
                   key={i.name}
-                  className="flex flex-row items-center gap-3 text-black"
                   href={i.path}
+                  className="flex flex-row items-center gap-3 text-black"
                 >
                   {i.icon}
                   {i.name}
                 </Link>
-              );
-            })}
+              )
+            )}
           </ul>
         </div>
         <span className="absolute bottom-10 flex w-full flex-row items-center justify-center text-black">
